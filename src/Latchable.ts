924834fa3a7ae515de23aa36ae53440776690d49
mweiczorek <nolawnchairs@gmail.com>
2021-11-09 16:00:42 +0700

    removed lockable, renamed file

--- src/Latchable.ts
+++ src/Latchable.ts
@@ -1,6 +1,4 @@
 
-import { Futures } from './Futures'
-
 /**
  * A holder for a value that can be locked
  *
@@ -63,61 +61,5 @@ export class Latchable<T> {
    */
   get value(): T {
     return this._value
-  }
-}
-
-export class Lockable<T> extends Latchable<T> {
-  constructor(value?: T) {
-    super(value)
-  }
-
-  /**
-   * Sets the value. Throws an error if attempting to set
-   * while locked
-   *
-   * @param {T} value the value to set
-   * @memberof Lockable
-   */
-  set(value: T) {
-    if (this._locked)
-      throw new Error('Lockable value is already locked')
-    this._value = value
-  }
-
-  /**
-   * Offer to set the value. Will wait until the value is unlocked,
-   * then will set the value. If a timeout is provided, the offer will
-   * reject if the value is not unlocked by the given timeout
-   *
-   * @param {T} value the value to set
-   * @param {number} [timeout] the amount of time in milliseconds to wait
-   * @memberof Lockable
-   */
-  async offer(value: T, timeout?: number) {
-    if (timeout) {
-      await Futures.waitUntilResolved(timeout, () => !this._locked)
-      this._value = value
-    } else {
-      await Futures.waitUntil(() => !this._locked)
-      this._value = value
-    }
-  }
-
-  /**
-   * Locks the value
-   *
-   * @memberof Lockable
-   */
-  lock() {
-    this._locked = true
-  }
-
-  /**
-   * Unlocks the value
-   *
-   * @memberof Lockable
-   */
-  unlock() {
-    this._locked = false
   }
 }
